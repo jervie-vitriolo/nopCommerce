5cf41e023c493aeca61224003d8d6657560e3794
DmitriyKulagin <kulagin87@gmail.com>
2018-05-28 11:32:59 +0300

    added settings for date picker "CountDisplayedYearsDatePicker"

--- src/Libraries/Nop.Services/Installation/CodeFirstInstallationService.cs
+++ src/Libraries/Nop.Services/Installation/CodeFirstInstallationService.cs
@@ -6121,7 +6121,8 @@ namespace Nop.Services.Installation
                 ExportImportUseDropdownlistsForAssociatedEntities = true,
                 ExportImportProductsCountInOneFile = 500,
                 ExportImportSplitProductsFile = false,
-                ExportImportRelatedEntitiesByName = true
+                ExportImportRelatedEntitiesByName = true,
+                CountDisplayedYearsDatePicker = 2
             });
 
             settingService.SaveSetting(new LocalizationSettings
